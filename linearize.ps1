67f713b604a854563d9684759967901e90d41606
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2022-02-14 14:03:32 -0700

    added linearize

---
+++ linearize.ps1
@@ -0,0 +1,16 @@
+# linearizes all xml files in a folder
+
+$count = 0
+$folder = 'C:\Users\Chris\Git\test'
+
+$children = Get-ChildItem -path $folder
+
+ForEach ($child in $children) {
+  $singlepath = $folder + '\' + $child
+  $content = Get-Content -path $singlepath
+  $mid = $content -join ''
+  $final = $mid -replace '\s+', ' '
+  $new = $folder + '\fixed' + $count + '.xml'
+  Set-Content -path $new -value $final
+  $count++
+}
