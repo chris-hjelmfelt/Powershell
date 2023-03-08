c990c8c5db788631750a905e0ed58664dd609524
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2023-03-08 14:20:22 -0600

    added mouse left click

--- little-bot.ps1
+++ little-bot.ps1
@@ -1,26 +1,28 @@
 
 Write-Host "Starting MouseMove"
-$MYJOB = Start-Job -ScriptBlock {
-  $MOVEMENTSIZE = 100
-  $SLEEPTIME = 1
-  $RUNCOUNTER = 0  
+$MYJOB = Start-Job -ScriptBlock {  
   Add-Type -AssemblyName System.Windows.Forms
-  while ($RUNCOUNTER -lt 2) {
-    $POSITION = [Windows.Forms.Cursor]::Position
-    $POSITION.x += $MOVEMENTSIZE
-    $POSITION.y += $MOVEMENTSIZE
-    [Windows.Forms.Cursor]::Position = $POSITION
-    Start-Sleep -Seconds $SLEEPTIME
-    $POSITION = [Windows.Forms.Cursor]::Position
-    $POSITION.x -= $MOVEMENTSIZE
-    $POSITION.y -= $MOVEMENTSIZE
-    [Windows.Forms.Cursor]::Position = $POSITION
-    Start-Sleep -Seconds $SLEEPTIME
-    $RUNCOUNTER += 1
-  }   
 
+  # mouse movement
+  # $MOVEMENTSIZE = 100
+  # $SLEEPTIME = 1
+  # $POSITION = [Windows.Forms.Cursor]::Position
+  # $POSITION.x += $MOVEMENTSIZE
+  # $POSITION.y += $MOVEMENTSIZE
+  # [Windows.Forms.Cursor]::Position = $POSITION
+  # Start-Sleep -Seconds $SLEEPTIME
+  # $POSITION = [Windows.Forms.Cursor]::Position
+  # $POSITION.x -= $MOVEMENTSIZE
+  # $POSITION.y -= $MOVEMENTSIZE
+  # [Windows.Forms.Cursor]::Position = $POSITION
+  # Start-Sleep -Seconds $SLEEPTIME
+
+  # Make application active
   [void] [System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic") 
-  [Microsoft.VisualBasic.Interaction]::AppActivate((Get-Process -Name notepad).MainWindowTitle)
-  [Windows.Forms.SendKeys]::SendWait("{X}")
-  [Windows.Forms.SendKeys]::SendWait("{ENTER}")
+  # [Microsoft.VisualBasic.Interaction]::AppActivate((Get-Process -Name notepad).MainWindowTitle)
+  [Microsoft.VisualBasic.Interaction]::AppActivate(11204) # find the App Id in Task Manager process details
+ 
+  # import mouse_event & left click
+  Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32 -Namespace W;
+  [W.U32]::mouse_event(6,0,0,0,0);
 }
