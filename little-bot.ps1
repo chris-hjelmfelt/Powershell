017f1a5a6e7008b3f985132cf0897cdc2c279d59
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2023-03-11 12:32:59 -0600

    get current position and printout, a round of action

--- little-bot.ps1
+++ little-bot.ps1
@@ -1,28 +1,47 @@
 
 Write-Host "Starting MouseMove"
 $MYJOB = Start-Job -ScriptBlock {  
+  #inport forms commands
   Add-Type -AssemblyName System.Windows.Forms
+  # import mouse_event
+  Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32 -Namespace W;
 
-  # mouse movement
-  # $MOVEMENTSIZE = 100
-  # $SLEEPTIME = 1
-  # $POSITION = [Windows.Forms.Cursor]::Position
-  # $POSITION.x += $MOVEMENTSIZE
-  # $POSITION.y += $MOVEMENTSIZE
-  # [Windows.Forms.Cursor]::Position = $POSITION
-  # Start-Sleep -Seconds $SLEEPTIME
-  # $POSITION = [Windows.Forms.Cursor]::Position
-  # $POSITION.x -= $MOVEMENTSIZE
-  # $POSITION.y -= $MOVEMENTSIZE
-  # [Windows.Forms.Cursor]::Position = $POSITION
-  # Start-Sleep -Seconds $SLEEPTIME
-
+  
   # Make application active
   [void] [System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic") 
   # [Microsoft.VisualBasic.Interaction]::AppActivate((Get-Process -Name notepad).MainWindowTitle)
   [Microsoft.VisualBasic.Interaction]::AppActivate(11204) # find the App Id in Task Manager process details
- 
-  # import mouse_event & left click
-  Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32 -Namespace W;
+
+  # Get current mouse position and print to file
+  # $X = [System.Windows.Forms.Cursor]::Position.X  #762
+  # $Y = [System.Windows.Forms.Cursor]::Position.Y  #412
+  # $RESULT = "X: $X, Y: $Y"
+  # Out-File -FilePath C:\Users\Legion4\Git\powershell\printout.txt -InputObject $RESULT
+
+  # mouse click
   [W.U32]::mouse_event(6,0,0,0,0);
+
+  # wait
+  Start-Sleep -Seconds 5  
+
+  # mouse movement
+  $POSITION = [Windows.Forms.Cursor]::Position
+  $POSITION.x = 762 - 100
+  $POSITION.y = 412 - 10
+  [Windows.Forms.Cursor]::Position = $POSITION
+   
+  # mouse click
+  [W.U32]::mouse_event(6,0,0,0,0);
+  
+  #wait
+  Start-Sleep -Seconds 30
+
+  # mouse movement
+  $POSITION.x += $MOVEMENTSIZE + 90
+  $POSITION.y += 5
+  [Windows.Forms.Cursor]::Position = $POSITION
+
+  # mouse click
+  [W.U32]::mouse_event(6,0,0,0,0);
+
 }
